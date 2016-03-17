5848c64b4945e4afde40acb1657be452ac160685
Qingsheng He <qingh@microsoft.com>
2016-03-17 15:31:00 +0800

    Some experiment.
    Change date display name
    Create the seed db records in movie table if no records.

--- src/ASP/Startup.cs
+++ src/ASP/Startup.cs
@@ -98,6 +98,8 @@ namespace ASP
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
 
         // Entry point for the application.
